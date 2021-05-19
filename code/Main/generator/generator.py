3e0bea9cd83570b9704500e0260395bf6d20acff
fender2758 <hyonjun.kang.00@gmail.com>
2021-05-20 05:47:23 +0900

    +Falling Slowy +Simple Main

--- code/Main/generator/generator.py
+++ code/Main/generator/generator.py
@@ -1,6 +1,7 @@
 from generator.hand import hand
 from music import music
 from fingering import fingering
+import guitarpro
 
 class Generator :
     def __init__(self, music_ = None) :
@@ -8,7 +9,21 @@ class Generator :
         self.__fingering = fingering.Fingering()
         self.__hand = hand.Hand(0, 0)
 
-        if music_ != None : 
+        if type(music_) == guitarpro.models.Song:
+            musics = []
+
+            for tr in music_.tracks:
+                for ms in tr.measures:
+                    for vc in ms.voices:
+                        for bt in vc.beats:
+                            beat = []
+                            for nt in bt.notes:
+                                note = [nt.string, nt.value, 0]
+                                beat.append(note)
+                            musics.append(beat)
+            self.set(musics)
+            
+        elif music_ != None : 
             self.set(music_)
             
     def set(self, music_) :
--- code/Main/main.py
+++ code/Main/main.py
@@ -4,28 +4,16 @@ import guitarpro
 
 Song = input()
 
-ProTab = guitarpro.parse("Resources\\twilight.gp5")
-
-music = []
-
-for tr in ProTab.tracks:
-    for ms in tr.measures:
-        for vc in ms.voices:
-            for bt in vc.beats:
-                beat = []
-                for nt in bt.notes:
-                    note = [nt.value, nt.string, 0]
-                    beat.append(note)
-                music.append(beat)
+ProTab = guitarpro.parse("Resources\\Falling slowly.gp5")
 
 
-g_generator = g.Generator(music)
 
-print("Before")
-printer.show(g_generator.get())
-"""
+g_generator = g.Generator(ProTab)
+
+#print("Before")
+#printer.show(g_generator.get())
 g_generator.generate()
 print("After")
-printer.show(g_generator.get(), 0)
-printer.show(g_generator.get(), 1)
-"""
+printer.show(g_generator.get())
+#printer.show(g_generator.get(), 0)
+#printer.show(g_generator.get(), 1)
