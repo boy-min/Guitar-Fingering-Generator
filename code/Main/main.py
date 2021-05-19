32ee158ff79e4ed76bd95f6be7124626fb1bf608
fender2758 <hyonjun.kang.00@gmail.com>
2021-05-20 02:54:55 +0900

    GuitarPro to Project

--- code/Main/main.py
+++ code/Main/main.py
@@ -1,11 +1,31 @@
 from generator import generator as g
 from printer import printer
+import guitarpro
 
-g_generator = g.Generator([[[1, 1, 0], [2, 3, 0]], [[3, 3, 0]]])
+Song = input()
+
+ProTab = guitarpro.parse("Resources\\twilight.gp5")
+
+music = []
+
+for tr in ProTab.tracks:
+    for ms in tr.measures:
+        for vc in ms.voices:
+            for bt in vc.beats:
+                beat = []
+                for nt in bt.notes:
+                    note = [nt.value, nt.string, 0]
+                    beat.append(note)
+                music.append(beat)
+
+
+g_generator = g.Generator(music)
+
 print("Before")
 printer.show(g_generator.get())
+"""
 g_generator.generate()
 print("After")
 printer.show(g_generator.get(), 0)
 printer.show(g_generator.get(), 1)
-
+"""
