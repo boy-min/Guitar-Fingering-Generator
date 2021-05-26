86172015c12015e12eacb663a0e8db07e995b0a7
foba1 <foba1@naver.com>
2021-05-26 17:37:10 +0900

    Delete class Fingering, and add __set_init_fingering() in Generator.

--- code/Main/generator/generator.py
+++ code/Main/generator/generator.py
@@ -1,24 +1,22 @@
 from generator.hand import hand
 from music import music
-from fingering import fingering
 import guitarpro
 
 class Generator :
-    def __init__(self, music_ = None) :
+    def __init__(self, music_ = None, size_ = None, length_ = None) :
         self.__music = music.Music()
-        self.__fingering = fingering.Fingering()
-        self.__hand = hand.Hand(0, 0)
+        self.__hand = hand.Hand(size_, length_)
 
-        if type(music_) == guitarpro.models.Song:
+        if type(music_) == guitarpro.models.Song :
             musics = []
 
-            for tr in music_.tracks:
-                for ms in tr.measures:
+            for tr in music_.tracks :
+                for ms in tr.measures :
                     measure = []
-                    for vc in ms.voices:
-                        for bt in vc.beats:
+                    for vc in ms.voices :
+                        for bt in vc.beats :
                             beat = []
-                            for nt in bt.notes:
+                            for nt in bt.notes :
                                 note = [nt.string, nt.value, 0]
                                 beat.append(note)
                             measure.append(beat)
@@ -36,12 +34,63 @@ class Generator :
         return self.__music
 
     def generate(self) :
-        #  generate efficient fingering and edit fingering in self.__music
-
-        #  set initial fingering
         for measure in self.__music.get() :
-            for notes_ in measure :
-                self.__fingering.set(notes_)
+            for idx, notes_ in enumerate(measure) :
+                self.__set_init_fingering(notes_)
 
-        #  generate efficient fingering
-        #  algorithms here
+    def __set_init_fingering(self, notes_) :
+        fret_list = []
+        overlap = -1
+        count = 0
+        for note1 in notes_.get() :
+            if note1.get("fret") != 0 :
+                if note1.get("fret") not in fret_list :
+                    fret_list.append(note1.get("fret"))
+                else :
+                    overlap = note1.get("fret")
+                    count = count + 1
+        fret_list.sort()
+        if len(fret_list) != 0 :
+            if overlap == -1 :
+                for note1 in notes_.get() :
+                    for idx in range(0,len(fret_list)) :
+                        if fret_list[idx] == note1.get("fret") :
+                            note1.set(None, None, idx + 1)
+                            break
+            else :
+                if count > 1 :
+                    for note1 in notes_.get() :
+                        for idx in range(0, len(fret_list)) :
+                            if fret_list[idx] == note1.get("fret") :
+                                note1.set(None, None, idx + 1)
+                                break
+                else :
+                    temp_list = []
+                    for note1 in notes_.get() :
+                        for idx in range(0,len(fret_list)) :
+                            if fret_list[idx] == note1.get("fret") :
+                                if fret_list[idx] > overlap :
+                                    note1.set(None, None, idx + 2)
+                                    break
+                                else :
+                                    note1.set(None, None, idx + 1)
+                                    if fret_list[idx] == overlap :
+                                        temp_list.append(note1)
+                                    break
+
+                    if len(temp_list) == 2 :
+                        if temp_list[0].get("string") > temp_list[1].get("string") :
+                            temp_list[1].set(None, None, temp_list[1].get("finger") + 1)
+                        else :
+                            temp_list[0].set(None, None, temp_list[0].get("finger") + 1)
+
+    def __get_distance(self, string_1, string_2, fret_1, fret_2) :
+        return abs(string_1 - string_2)**2 + abs(fret_1 - fret_2)**2
+
+    def __get_direction(self, fret_1, fret_2):
+        if fret_1 > fret_2 :
+            return -1
+        elif fret_1 == fret_2 :
+            return 0
+        else :
+            return 1
