859c79be4c29de15084ea680338d661ace1681b2
foba1 <foba1@naver.com>
2021-05-30 21:54:28 +0900

    Fix all error in generate() - dp.

--- code/Main/generator/generator.py
+++ code/Main/generator/generator.py
@@ -6,7 +6,7 @@ class Generator:
     def __init__(self, music_ = None, size_ = None, length_ = None):
         self.__music = music.Music()
         self.__hand = hand.Hand(size_, length_)
-        self.__weight = [1000, 0, 0, 0]
+        self.__weight = [1000, 100, 50, 10]
 
         if type(music_) == guitarpro.models.Song:
             musics = []
@@ -68,27 +68,31 @@ class Generator:
                         min_ = 2147483647
                         for finger2 in range(4 ** len(music_[i+1].get())):
                             fingers2 = []
-                            for k in range(len(music_[i+1].get())):
-                                fingers2.append((finger2 % (4 ** (k + 1))) // (4 ** k))
+                            for j in range(len(music_[i+1].get())):
+                                fingers2.append((finger2 % (4 ** (j + 1))) // (4 ** j))
                             dif = dp[i+1][finger2][0] + self.__difficulty(None,None,music_[i+1],fingers2)
                             if min_ > dif:
                                 min_ = dif
                                 dp[i][4096][1] = finger2
                         dp[i][4096][0] = min_
                 else:
-                    for finger1 in range(4**len(music_[i].get())):
-                        fingers1 = []
-                        for j in range(len(music_[i].get())):
-                            fingers1.append((finger1 % (4 ** (j + 1))) // (4 ** j))
-                        if len(music_[i+1].get()) == 0:
+                    if len(music_[i + 1].get()) == 0:
+                        for finger1 in range(4 ** len(music_[i].get())):
+                            fingers1 = []
+                            for j in range(len(music_[i].get())):
+                                fingers1.append((finger1 % (4 ** (j + 1))) // (4 ** j))
                             dp[i][finger1][0] = dp[i+1][4096][0] + self.__difficulty(music_[i],fingers1,None,None)
                             dp[i][finger1][1] = 4096
-                        else:
+                    else:
+                        for finger1 in range(4**len(music_[i].get())):
+                            fingers1 = []
+                            for j in range(len(music_[i].get())):
+                                fingers1.append((finger1 % (4 ** (j + 1))) // (4 ** j))
                             min_ = 2147483647
                             for finger2 in range(4**len(music_[i+1].get())):
                                 fingers2 = []
-                                for k in range(len(music_[i+1].get())):
-                                    fingers2.append((finger2 % (4 ** (k + 1))) // (4 ** k))
+                                for j in range(len(music_[i+1].get())):
+                                    fingers2.append((finger2 % (4 ** (j + 1))) // (4 ** j))
                                 dif = dp[i+1][finger2][0] + self.__difficulty(music_[i],fingers1,music_[i+1],fingers2)
                                 if min_ > dif:
                                     min_ = dif
@@ -118,7 +122,7 @@ class Generator:
                 if len(music_[i].get()) == 0:
                     fingers = []
                     finger_list.append(fingers)
-                    idx = 4096
+                    idx = dp[i][4096][1]
                 else:
                     fingers = []
                     for j in range(len(music_[i].get())):
@@ -132,28 +136,46 @@ class Generator:
 
     def __difficulty(self, notes_1, finger_list_1, notes_2, finger_list_2):
         difficulty = 0
-        """
         difficulty += self.__check_highcode(notes_1, finger_list_1, notes_2, finger_list_2)
         if difficulty >= self.__weight[0]:
             return difficulty
-        elif finger_list_1 is not None :
-            if 4 in finger_list_1:
-                print("?")"""
         if notes_1 is None and finger_list_1 is None:
-            if 4 in finger_list_2:
-                return self.__weight[0]
+            if 3 in finger_list_2:
+                difficulty += self.__weight[1]
+            elif 2 in finger_list_2:
+                difficulty += self.__weight[2]
+            elif 1 in finger_list_2:
+                difficulty += self.__weight[3]
             else:
-                return difficulty
+                difficulty += 1
         elif notes_2 is None and finger_list_2 is None:
-            if 4 in finger_list_1:
-                return self.__weight[0]
-            else :
-                return difficulty
-        else :
-            if 4 in finger_list_1:
-                return self.__weight[0]
+            if 3 in finger_list_1:
+                difficulty += self.__weight[1]
+            elif 2 in finger_list_1:
+                difficulty += self.__weight[2]
+            elif 1 in finger_list_1:
+                difficulty += self.__weight[3]
             else:
-                return difficulty
+                difficulty += 1
+        else :
+            if 3 in finger_list_1:
+                difficulty += self.__weight[1]
+            elif 2 in finger_list_1:
+                difficulty += self.__weight[2]
+            elif 1 in finger_list_1:
+                difficulty += self.__weight[3]
+            else:
+                difficulty += 1
+            if 3 in finger_list_2:
+                difficulty += self.__weight[1]
+            elif 2 in finger_list_2:
+                difficulty += self.__weight[2]
+            elif 1 in finger_list_2:
+                difficulty += self.__weight[3]
+            else:
+                difficulty += 1
+        return difficulty
+
 
     def __check_twist(self, notes_1, finger_list_1, notes_2, finger_list_2):
         difficulty = 0
