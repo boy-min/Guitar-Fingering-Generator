1e8fa3e0130cc0792519fb6e2acb6baf43357641
foba1 <foba1@naver.com>
2021-05-31 23:39:54 +0900

    Add check_highcode(), check_twist(), get_pos(), get_dif().

--- code/Main/generator/generator.py
+++ code/Main/generator/generator.py
@@ -6,7 +6,7 @@ class Generator:
     def __init__(self, music_ = None, size_ = None, length_ = None):
         self.__music = music.Music()
         self.__hand = hand.Hand(size_, length_)
-        self.__weight = [1000, 100, 50, 10]
+        self.__weight = [1000]
 
         if type(music_) == guitarpro.models.Song:
             musics = []
@@ -135,4 +135,82 @@ class Generator:
                 note_.set(None, None, finger_list[idx1][idx2] + 1)
 
     def __difficulty(self, notes_1, finger_list_1, notes_2, finger_list_2):
+        difficulty = 0
+        difficulty += self.__check_highcode(notes_1, finger_list_1)
+        difficulty += self.__check_highcode(notes_2, finger_list_2)
+        difficulty += self.__check_twist(notes_1, finger_list_1)
+        difficulty += self.__check_twist(notes_2, finger_list_2)
+        if difficulty >= self.__weight[0]:
+            return difficulty
+
+        if notes_1 is not None and notes_2 is None:
+            difficulty += self.__get_dif(notes_1, finger_list_1)
+        elif notes_1 is None and notes_2 is not None:
+            difficulty += self.__get_dif(notes_2, finger_list_2)
+        else:
+            difficulty += self.__get_dif(notes_1, finger_list_1)
+            difficulty += self.__get_dif(notes_2, finger_list_2)
+            pos_of_hand_1 = self.__get_pos(notes_1, finger_list_1)
+            pos_of_hand_2 = self.__get_pos(notes_2, finger_list_2)
+            # 여러 정보 (손의 중심에 따른 이동 거리, 손가락들 사이의 거리, 손가락들 사이의 모양이 변하는 정도 등)
+            # 를 이용하여 운지법의 변화에 따른 difficulty 의 값을 반환하는 알고리즘
+
+        return difficulty
+
+    def __get_pos(self, notes, finger_list):
+        return 0
+        # 손의 중심 번호를 반환하는 알고리즘
+
+    def __get_dif(self, notes, finger_list):
+        return 0
+        # 하나의 notes에 대하여 얼마나 운지하기 어려운지 판단하는 알고리즘
+
+    def __check_twist(self, notes, finger_list):
+        if notes is not None and finger_list is not None:
+            temp_list = []
+            for idx, note_ in enumerate(notes.get()):
+                temp_list.append([note_.get("fret"), finger_list[idx]])
+            temp_list.sort()
+            for idx in range(len(temp_list) - 1):
+                if temp_list[idx][0] < temp_list[idx+1][0]:
+                    if temp_list[idx][1] >= temp_list[idx+1][1]:
+                        return self.__weight[0]
+                else:
+                    if temp_list[idx][1] != temp_list[idx+1][1]:
+                        return self.__weight[0]
+        return 0
+
+    def __check_highcode(self, notes, finger_list):
+        if notes is not None and finger_list is not None:
+            is_notes_highcode = False
+            finger_count = [0, 0, 0, 0]
+            for finger in finger_list:
+                finger_count[finger] += 1
+            for idx in range(4):
+                if finger_count[idx] > 1:
+                    if idx == 0:
+                        is_notes_highcode = True
+                    else:
+                        return self.__weight[0]
+
+            if is_notes_highcode:
+                string_list = []
+                fret = -1
+                for idx, note_ in enumerate(notes.get()):
+                    if fret == -1:
+                        if finger_list[idx] == 0:
+                            fret = note_.get("fret")
+                            string_list.append(note_.get("string"))
+                    else:
+                        if finger_list[idx] == 0:
+                            if fret != note_.get("fret"):
+                                return self.__weight[0]
+                            if note_.get("string") not in string_list:
+                                string_list.append(note_.get("string"))
+
+                string_list.sort()
+                for idx in range(len(string_list) - 1):
+                    if string_list[idx] != string_list[idx + 1] - 1:
+                        return self.__weight[0]
+
         return 0
