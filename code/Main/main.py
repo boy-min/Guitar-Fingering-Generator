f77208e260bfadf16c39686596aee357ff82e8ff
foba1 <foba1@naver.com>
2021-06-01 15:54:44 +0900

    Revert "Fix index error."
    This reverts commit dc352983d3e82cb6022622a62c61a528286b5a87.

--- code/Main/main.py
+++ code/Main/main.py
@@ -49,8 +49,7 @@ for id_, measure in enumerate(notes.get()):
                 finger = guitarpro.Fingering.little
             else :
                 finger = guitarpro.Fingering.open
-            if ProTab.tracks[0].isPercussionTrack:
-                ProTab.tracks[0].measures[id_].voices[0].beats[idx].notes[index].effect.leftHandFinger = finger
+            ProTab.tracks[0].measures[id_].voices[0].beats[idx].notes[index].effect.leftHandFinger = finger
 guitarpro.write(ProTab, "Resources\\Finger_"+Song)
 
 print("\n"+"Success to write file : Finger_"+Song)
