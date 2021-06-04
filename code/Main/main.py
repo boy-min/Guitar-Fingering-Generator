4894890d6804f3ffac81dc9c8e195c0741f8238f
fender2758 <hyonjun.kang.00@gmail.com>
2021-06-04 20:45:58 +0900

    Revert "follow defficulty"
    This reverts commit efb5d7d42e173aeeb2c89e252475914c49d2a5a5.

--- code/Main/main.py
+++ code/Main/main.py
@@ -33,12 +33,13 @@ g_generator = g.Generator(ProTab, size, length)
 g_generator.generate()
 
 notes = g_generator.get()
-for id_, measure in enumerate(notes.get()):
-    for idx, beat in enumerate(measure):
-        for index, note in enumerate(beat.get()):
+for id, measure in enumerate(notes.get()) :
+    for idx, beat in enumerate(measure) :
+        for index, note in enumerate(beat.get()) :
             fin = note.get()["finger"]
-            finger = 0
-            if fin == 1 :
+            if fin == 0 :
+                finger = guitarpro.Fingering.open
+            elif fin == 1 :
                 finger = guitarpro.Fingering.index
             elif fin == 2 :
                 finger = guitarpro.Fingering.middle
@@ -46,9 +47,7 @@ for id_, measure in enumerate(notes.get()):
                 finger = guitarpro.Fingering.annular
             elif fin == 4 :
                 finger = guitarpro.Fingering.little
-            else :
-                finger = guitarpro.Fingering.open
-            ProTab.tracks[0].measures[id_].voices[0].beats[idx].notes[index].effect.leftHandFinger = finger
+            ProTab.tracks[0].measures[id].voices[0].beats[idx].notes[index].effect.leftHandFinger = finger
 guitarpro.write(ProTab, "Resources\\Finger_"+Song)
 
 print("\n"+"Success to write file : Finger_"+Song)
